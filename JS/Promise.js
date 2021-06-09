5539561fc95fb57af4da7a68be851a87c374ba5a
biscotti <15773205162@163.com>
2021-06-09 13:51:47 +0800

    feator:结构修改

--- JS/Promise.js
+++ JS/Promise.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-17 16:25:22
- * @LastEditTime: 2021-06-01 15:57:01
+ * @LastEditTime: 2021-06-01 19:16:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /methodsAccumulation/JS方法/Promise.js
@@ -94,4 +94,3 @@ console.log(Reflect.apply(Math.floor, undefined, [1.75]));
 console.log(Function.prototype.apply.call(Math.floor, undefined, [1.75]));
 // expected output: 1
 
-2222
